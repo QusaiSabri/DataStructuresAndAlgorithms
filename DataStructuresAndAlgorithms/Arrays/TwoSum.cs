d5a9c243727adf86a7c61768732e3405762c96cf
Qusai Sabri <qusai.sabri19@gmail.com>
2023-05-13 19:44:46 -0500

    Add TwoSum solution and organize the program.cs

--- DataStructuresAndAlgorithms/Arrays/TwoSum.cs
+++ DataStructuresAndAlgorithms/Arrays/TwoSum.cs
@@ -1,11 +1,41 @@
 ﻿namespace DataStructuresAndAlgorithms.Arrays
 {
     /// <summary>
-    /// Two Sum
+    /// Two Sum --> https://leetcode.com/problems/two-sum/
     /// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
     // You may assume that each input would have exactly one solution, and you may not use the same element twice.
+    //
+    // Example 1:
+    // Input: nums = [2,7,11,15], target = 9
+    // Output: [0,1]
+    // Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
     /// </summary>
-    public class TwoSum
+    public static class TwoSum
     {
+        /// <summary>
+        /// Solves the Two Sum problem using a hash map.
+        /// <para>Time complexity: O(n), where n is the number of elements in the array, because we make a single pass through the array.</para>
+        /// <para>Space complexity: O(n), because in the worst case, we would need to store all numbers in the hash map.</para>
+        /// </summary>
+        public static int[] SolveWithHashMap(int[] nums, int target)
+        {
+            var numIndices = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                var complement = target - nums[i];
+
+                if (numIndices.ContainsKey(complement))
+                {
+                    return new int[] { numIndices[complement], i };
+                }
+                else
+                {
+                    numIndices.Add(nums[i], i);
+                }
+            }
+
+            return new int[] { };
+        }
     }
 }
--- DataStructuresAndAlgorithms/Program.cs
+++ DataStructuresAndAlgorithms/Program.cs
@@ -1,14 +1,34 @@
 ﻿using DataStructuresAndAlgorithms;
+using DataStructuresAndAlgorithms.Arrays;
 
-int[] profits = { 1, 6, 10, 16 };
-int[] weights = { 1, 2, 3, 5 };
-var knapsack = new DynamicProgramming.Knapsack();
 
-var resDP = knapsack.DP(profits, weights, 7);
-var resRecWithMem = knapsack.RecursiveWithMemoization(profits, weights, 7);
-var resRec = knapsack.Recursive(profits, weights, 7);
+TestTwoSum();
 
-Console.WriteLine("Total profit --> " + resDP);
-Console.WriteLine("Total profit --> " + resRecWithMem);
-Console.WriteLine("Total profit --> " + resRec);
+
+static void TestTwoSum()
+{
+    // Test case
+    int[] nums = { 2, 7, 11, 15 };
+    int target = 9;
+
+    var result = TwoSum.SolveWithHashMap(nums, target);
+    Console.WriteLine(string.Join(", ", result));
+}
+
+static void TestKnapsack()
+{
+    int[] profits = { 1, 6, 10, 16 };
+    int[] weights = { 1, 2, 3, 5 };
+    var knapsack = new DynamicProgramming.Knapsack();
+
+    var resDP = knapsack.DP(profits, weights, 7);
+    var resRecWithMem = knapsack.RecursiveWithMemoization(profits, weights, 7);
+    var resRec = knapsack.Recursive(profits, weights, 7);
+
+    Console.WriteLine("Total profit --> " + resDP);
+    Console.WriteLine("Total profit --> " + resRecWithMem);
+    Console.WriteLine("Total profit --> " + resRec);
+
+}
+
 
