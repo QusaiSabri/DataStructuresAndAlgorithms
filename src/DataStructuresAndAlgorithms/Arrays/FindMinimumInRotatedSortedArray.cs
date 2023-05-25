316ef92c4ae92f7757d95b649fca35489033b4b0
Qusai Sabri <qusai.sabri19@gmail.com>
2023-05-25 00:28:59 -0500

    Add "Find Minimum in Rotated Sorted Array"

---
+++ src/DataStructuresAndAlgorithms/Arrays/FindMinimumInRotatedSortedArray.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Numerics;
+
+namespace DataStructuresAndAlgorithms.Arrays
+{
+    //Example [4,5,6,7,0,1,2]
+    //Example [0,1,2,4,5,6,7]
+    public class FindMinimumInRotatedSortedArray
+	{
+        /// <summary>
+        /// Solves the "Find Minimum in Rotated Sorted Array" problem.
+        /// <para>Time Complexity: O(log n), because we are using a binary search to halve the search space at every step.</para>
+        /// <para>Space Complexity: O(1), since we are using a constant amount of space.</para>
+        /// </summary>
+        public int SolveWithBinarySearch(int[] nums)
+        {
+            var left = 0;
+            var right = nums.Length - 1;
+
+            while(left < right)
+            {
+                var mid = left + (right - left) / 2;
+
+                if (nums[mid] > nums[right])
+                {
+                    left = mid + 1;
+                } else
+                {
+                    right = mid;
+                }
+
+            }
+
+            return nums[left];
+        }
+    }
+}
+
---
+++ test/DataStructuresAndAlgorithms.Tests.Unit/Arrays/FindMinimumInRotatedSortedArrayTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using DataStructuresAndAlgorithms.Arrays;
+using FluentAssertions;
+
+namespace DataStructuresAndAlgorithms.Tests.Unit.Arrays
+{
+	public class FindMinimumInRotatedSortedArrayTests
+	{
+        [Theory]
+        [InlineData(new int[] { 3, 4, 5, 1, 2 }, 1)]
+        [InlineData(new int[] { 4, 5, 6, 7, 0, 1, 2 }, 0)]
+        [InlineData(new int[] { 11, 13, 15, 17 }, 11)]
+        public void FindMin_ShouldReturnMinimumElement(int[] nums, int expected)
+        {
+            // Arrange
+            var findMinimumInRotated = new FindMinimumInRotatedSortedArray();
+
+            // Act
+            var result = findMinimumInRotated.SolveWithBinarySearch(nums);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
+
