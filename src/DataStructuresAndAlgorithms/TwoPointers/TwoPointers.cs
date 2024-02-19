0e8fa3be69176ac83b55949cae58e6c785282794
Qusai Sabri <qusai.sabri19@gmail.com>
2024-02-18 23:28:13 -0600

    Two Sum II question.

--- src/DataStructuresAndAlgorithms/TwoPointers/TwoPointers.cs
+++ src/DataStructuresAndAlgorithms/TwoPointers/TwoPointers.cs
@@ -43,5 +43,17 @@
 
             return true;
         }
+
+        // Two Sum II - Input Array Is Sorted
+        // Given an array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number.
+        // Return the indices of the two numbers (1-indexed) as an integer array answer of size 2, where 1 <= answer[0] < answer[1] <= numbers.length.
+        // The tests are generated such that there is exactly one solution. You may not use the same element twice.
+        // Example 1: Input: numbers = [2,7,11,15], target = 9
+        // Output: [1,2]
+
+        public int[] TwoSum(int[] numbers, int target)
+        {
+
+        }
     }
 }
